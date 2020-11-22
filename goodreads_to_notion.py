370e5e41426123a88114d8e824707f86d651488a
Gaurav <gauravshetty4@gmail.com>
2020-11-22 15:38:54 +0530

    Converted to a function that gets book details from a goodreads id and notion credentials

--- goodreads_to_notion.py
+++ goodreads_to_notion.py
@@ -8,23 +8,23 @@ from notion.client import NotionClient
 import argparse
 import sys
 
-if __name__ == "__main__":
-    description = "Run notion-py client smoke tests"
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--book", dest="book_id", help="Goodreads Book ID", required=True, type=int)
-    args = parser.parse_args()
 
+def goodreads_book_to_notion(book_id, notion_token, notion_page_url):
+    # description = "Run notion-py client smoke tests"
+    # parser = argparse.ArgumentParser(description=description)
+    # parser.add_argument("--book", dest="book_id", help="Goodreads Book ID", required=True, type=int)
+    # args = parser.parse_args()
+    #
+    # book_id = args.book_id
+    # if not book_id:
+    #     print(
+    #         "Must pass book id"
+    #     )
+    #     sys.exit(1)
 
-    book_id = args.book_id
-    if not book_id:
-        print(
-            "Must pass book id"
-        )
-        sys.exit(1)
+    client = NotionClient(token_v2=notion_token)
 
-    client = NotionClient(token_v2="a46f9b8ce309ada8e9b67137354230dffcdeee3d19ec3ef417c299d31def7e8b5419991ef894b77bd47618d615fc2d1aebf1da4e262370b85407887b4ca8a396ce9b321745a4500c6245879993b1")
-
-    cv = client.get_collection_view("https://www.notion.so/gauravshetty4/52d7820f75574d40b41b63cba842679c?v=551574aa4359418eb63a77d5223a3aff")
+    cv = client.get_collection_view(notion_page_url)
 
     goodreads = Books()
     book = goodreads.get_book_details(book_id)
@@ -38,6 +38,7 @@ if __name__ == "__main__":
         row.Book_Id = book["Book Id"]
         row.Exclusive_Shelf = "to-read"
         row.Number_of_Pages = book["Number of Pages"]
+        row.Cover = book["Cover"]
     except AttributeError:
         row = cv.collection.add_row()
         row.title = book["Title"]
@@ -45,5 +46,6 @@ if __name__ == "__main__":
         row.Book_Id = book["Book Id"]
         row.Exclusive_Shelf = "to-read"
         row.Number_of_Pages = book["Number of Pages"]
+        row.Cover = book["Cover"]
 
     print(row)
